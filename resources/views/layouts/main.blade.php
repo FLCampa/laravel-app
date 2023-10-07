0bb6c8fc0c61252ab487c10f0bafe4d211a90faf
FLCampa <flcampanelli@gmail.com>
2023-10-07 15:12:43 -0300

    feat: implements flash messages, image upload, show function and store array of items

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -47,7 +47,17 @@
     </nav>
   </header>
 
-  @yield('content')
+  <main>
+    <div class="container-fluid">
+      <div class="row">
+        @if(session('msg'))
+        <p class="msg">{{ session('msg') }}</p>
+        @endif
+
+        @yield('content')
+      </div>
+    </div>
+  </main>
 
   <footer>
     <p>Laravel app &copy; 2023</p>
